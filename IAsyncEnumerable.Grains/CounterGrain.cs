9342ec0427b0ee74e46f01f187af2b9d532df08d
Pablo Vivera <pablo.vivera.salad@gmail.com>
2024-04-24 11:50:38 -0400

    Empty IAsyncEnumerable

--- IAsyncEnumerable.Grains/CounterGrain.cs
+++ IAsyncEnumerable.Grains/CounterGrain.cs
@@ -9,18 +9,11 @@ namespace IAsyncEnumerable.Grains
 
         public override async Task OnActivateAsync(CancellationToken cancellationToken)
         {
-            var t= Task.Run(async () =>
-            {
-                for (int i = 0; i < 100; i++)
-                {
-                    await _jobChannel.Writer.WriteAsync(i, cancellationToken);
-                    await Task.Delay(1000, cancellationToken);
-                }
-            });
         }
 
         public async IAsyncEnumerable<int> GetCount()
         {
+            Console.WriteLine("GetCount started");
             try
             {
                 await foreach (var i in _jobChannel.Reader.ReadAllAsync())
